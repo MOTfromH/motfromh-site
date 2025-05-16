b0c0d88af250085c93a7a8ce92235d6c1a24bfe5
mot <m.tidjani@web.de>
2025-05-16 16:18:09 +0200

    feat(vue): implement first data fetch from API

--- frontend/nuxt-app/app.vue
+++ frontend/nuxt-app/app.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <Navbar />
-    <Banner />
+    <!-- <Navbar />
+    <Banner /> -->
+    <NuxtPage />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
---
+++ frontend/nuxt-app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Raw API Output</h1>
+  <pre>{{ entries }}</pre>
+  <div v-if="error" class="error">Error: {{ error.message }}</div>
+</template>
+
+<script setup lang="ts">
+// 1) first data fetch
+const { data: entries, error } = await useFetch(
+  "http://localhost:8001/api/v1/content/"
+);
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
